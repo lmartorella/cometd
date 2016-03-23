3c156bf603d69c35f9d5c64d2a86974a1657f4f6
lmartorella <lmartorella@iontrading.com>
2016-03-23 12:33:55 +0100

    - Made a custom build that doesn't depends on jQuery - Added resulting build for npm

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+    var concatList = [
+        "cometd-javascript/umd/cometd-header.js",
+        "cometd-javascript/common/src/main/webapp/js/cometd/cometd.js",
+        "cometd-javascript/common/src/main/webapp/js/cometd/AckExtension.js",
+        "cometd-javascript/common/src/main/webapp/js/cometd/TimeSyncExtension.js",
+        "cometd-javascript/common/src/main/webapp/js/cometd/BinaryExtension.js"
+    ];
+
+	grunt.initConfig({
+		concat: {
+			cometd: {
+				src: concatList,
+				dest: "dist/temp/cometd-all-concat.js"
+			}
+		},
+		umd: {
+			cometd: {
+				options: {
+					src: "dist/temp/cometd-all-concat.js",
+					dest: "dist/cometd-all.js",
+					objectToExport: "window.org.cometd",
+					amdModuleId: 'cometd',
+					globalAlias: 'cometd'
+				}
+			}
+		},
+		uglify: {
+			cometd: {
+				files: {
+					"dist/cometd-all-min.js": ["dist/cometd-all.js"]
+				}
+			}
+		}
+	});
+
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-umd");
+    grunt.registerTask("build", ["concat", "umd", "uglify"]);
+};
---
+++ cometd-javascript/umd/cometd-header.js
@@ -0,0 +1,8 @@
+
+// The extensions cannot be simply concatenated since they use a non-standard AMD usage (multiple def)
+
+// Disable AMD internally
+var org = org || {};
+org.cometd = org.cometd || {};
+var define = undefined;
+
